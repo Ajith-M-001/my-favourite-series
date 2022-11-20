519cff5ccc02e9dfaa396ded95c91abdece342a7
Ajith M <ajithgiri4455@gmail.com>
2022-11-20 20:48:41 +0530

    myfirst commit

---
+++ src/Sdata.jsx
@@ -0,0 +1,70 @@
+const Sdata =[
+
+    {
+        sname:"Game of Thrones", 
+        imgsrc:"https://static.posters.cz/image/750webp/135455.webp" ,
+        title:"My Favorite Series" ,
+        link:"https://goku.to/series/watch-game-of-thrones-39539",
+    },
+    {
+        sname:"Breaking Bad" ,
+        imgsrc:"https://flxt.tmsimg.com/assets/p185846_b_v8_ad.jpg",
+        title:"My Favorite Series ", 
+        link:"https://goku.to/series/watch-breaking-bad-39506",
+    },
+    {
+        sname:"Dark", 
+        imgsrc:"https://fr.web.img4.acsta.net/pictures/17/11/10/12/18/2448823.jpg",
+         title:"My Favorite Series" ,
+         link:"https://goku.to/series/watch-dark-38935",
+    },
+    {
+        sname:"House of the Dragon", 
+        imgsrc:"https://assets.gadgets360cdn.com/pricee/assets/product/202208/House-of-the-Dragon-poster_1659955685.jpeg",
+        title:"My Favorite Series" ,
+        link:"https://goku.to/series/watch-house-of-the-dragon-84837",
+    },
+    {
+        sname:"Friends", 
+        imgsrc:"https://flxt.tmsimg.com/assets/p183931_b_v8_ac.jpg",
+        title:"My Favorite Series" ,
+        link:"https://goku.to/series/watch-friends-39473",
+    },
+    {
+        sname:"Attack on Titan", 
+        imgsrc:"https://flxt.tmsimg.com/assets/p10701949_b_v8_ah.jpg",
+        title:"My Favorite Series" ,
+        link:"https://goku.to/series/watch-attack-on-titan-39339",
+    },
+
+    {
+        sname:"Stranger Things ",
+        imgsrc:"https://culturess.com/files/2022/05/amazon-TrendsInternational-stranger-things-season-4-poster-creel-house.jpg",
+        title:"My Favorite Series" ,
+        link:"https://goku.to/series/watch-stranger-things-39444",
+    },
+
+    {
+        sname:"The Boys ",
+        imgsrc:"https://m.media-amazon.com/images/M/MV5BOTEyNDJhMDAtY2U5ZS00OTMzLTkwODktMjU3MjFkZWVlMGYyXkEyXkFqcGdeQXVyMjkwOTAyMDU@._V1_.jpg",
+        title:"My Favorite Series" ,
+        link:"https://goku.to/series/watch-the-boys-33895",
+    },
+
+    {
+        sname:"Loki", 
+        imgsrc:"https://oyster.ignimgs.com/wordpress/stg.ign.com/2021/03/Ewxm7XEWUAsGVjE-720x1066.jpg",
+        title:"My Favorite Series" ,
+        link:"https://goku.to/series/watch-loki-42258",
+    },
+
+    {
+        sname:"Rick and Morty", 
+        imgsrc:"https://m.media-amazon.com/images/M/MV5BZjRjOTFkOTktZWUzMi00YzMyLThkMmYtMjEwNmQyNzliYTNmXkEyXkFqcGdeQXVyNzQ1ODk3MTQ@._V1_.jpg",
+        title:"My Favorite Series" ,
+        link:"https://goku.to/series/watch-rick-and-morty-39480",
+    },
+
+]
+
+export default Sdata;
